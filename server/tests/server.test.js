a87f4c32b237595ce8c82e726756d5b2fae575b1
pratibha hankare <pratibha.bittint@gmail.com>
2018-02-21 17:24:42 +0530

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,20 +6,30 @@ const request=require('supertest');
 const {app}=require('./../server');
 const {Todo}=require('./../models/todo');
 
+/*adding the data that is required to fetch from in some test suites*/
+const todos=[
+  {
+    text:'First test todo'
+  },{
+    text:'Second test todo'
+  }
+];
 /**dropping documents before the test suite is ran **/
 beforeEach((done)=>{
   Todo.remove({}).then(
-    ()=>done()
-  );
+    ()=>{
+    return Todo.insertMany(todos);
+  }
+).then(
+  ()=>done()
+);
 });
-
-
 /*describe() ->allows to group the multiple routes*/
 //describe block for Todos
 describe(('POST /todos'),()=>{
   //starting the test case
   /*checking if test data is been inserted correctly or not*/
-/*  it('should create a new todo',(done)=>{
+ it('should create a new todo',(done)=>{
     var text='Test todo text';
     request(app)
       .post('/todos')
@@ -32,7 +42,7 @@ describe(('POST /todos'),()=>{
         if(err){
           return done(err);
         }
-        Todo.find().then(
+        Todo.find({text}).then(
           (todos)=>{
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -41,7 +51,7 @@ describe(('POST /todos'),()=>{
         ).catch((e)=>done(e))
       });
   });
-*/
+
   /*challenge:- checking if there is invalid data is entered*/
   it('should not create todo with invalid body data',(done)=>{
       request(app)
@@ -56,7 +66,7 @@ describe(('POST /todos'),()=>{
             //cross checking with DB
             Todo.find().then(
               (todos)=>{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
               }
             ).catch(
@@ -64,6 +74,20 @@ describe(('POST /todos'),()=>{
             );
           });
   });
+});
 
-
+//test suites for GET
+describe('GET /todos',()=>{
+  //test case
+  it('should get all todos',(done)=>{
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(
+        (res)=>{
+          expect(res.body.todos.length).toBe(2);
+        }
+      )
+      .end(done);
+  });
 });
