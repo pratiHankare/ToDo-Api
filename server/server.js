8db07ec9d2874308de0afbbf29c65d9a0a83d7f7
pratibha hankare <pratibha.bittint@gmail.com>
2018-02-21 12:56:34 +0530

    connection to Mongoose,creating route+middleware+outputing in console

--- server/server.js
+++ server/server.js
@@ -1,66 +1,34 @@
-//loading monogoose
-var mongoose= require('mongoose');
-//inorder to use promise in our callback functions we need to configure
-mongoose.Promise = global.Promise;
-//the connection string to the db
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+/*Lib Imports*/
+var express=require('express');
+var bodyParser=require('body-parser');
 
-//creating a model .model helps in keeping the collections more organised
-/*this basically defines the What collection shuld contain(col) along with col defination(like what kind of col shuld accept input as)*/
-/*var Todo=mongoose.model('Todo',{
-  text:{
-    type:String,
-    required:true,
-    trim:true,
-    minlength:1
-  },
-  completed:{
-    type:Boolean,
-    default:false
-  },
-  completedAt:{
-    type:Number,
-    default:null
-  }
-});*/
-/*
-//creating the instance of above model:Todo
-var newTodo=new Todo({
-  text:'Cook dinner'
-});
-//the above instance with data need to saved
-//below we are saving instance if not saved than pop an error
-newTodo.save().then(
-  (doc)=>{ console.log('Saved todo',doc)},
-  (e)=>{console.log('Unable to save todo')}
-);
-*/
-/*challenge
-var secondTodo=new Todo({
-  text:'have tea',
-});
+/*Local imports*/
+//including the connection string in the files
+var {mongoose} =require ('./db/mongoose');
+/*including custome files*/
+var {Todo}=require('./models/todo');
+var {User}=require('./models/user');
 
-secondTodo.save().then(
-  (doc)=>{ console.log('saved todo',doc);},
-  (e)=>{ console.log('Unable to save todo',e); }
-);*/
+//intialising var that is going to store app
+var app = express();
 
-/*Challenge -> create a user model that accepts email nd store in DB*/
-/*Model*/
-var users=mongoose.model('Users',{
-  email:{
-    type:String,
-    minlength:1,
-    required:true,
-    trim:true
-  }
+//intialising middleware->using middleware from body-parser
+app.use(bodyParser.json());
+
+//setting up route with post
+app.post('/todos',(req,res)=>{
+  //console.log(req.body);
+  /*saving the input in the DB*/
+  var todo= new Todo(
+    {
+      text:req.body.text
+    });
+  todo.save().then(
+    (doc)=>{ res.send(doc);},
+    (e)=>{ res.status(400).send(e);}
+  );
 });
-/*create a instance to insert te input*/
-var user=new users({
-  email:' prati@gmail.com      '
+//inorder to run the app on Local it has listen by server at some port
+app.listen(3000,()=>{
+  console.log('Started on Port 3000.');
 });
-/*saving the data inside the db*/
-user.save().then(
-  (doc)=>{console.log(JSON.stringify(doc,undefined,2));},
-  (e)=>{console.log('unable to save',e)}
-);
